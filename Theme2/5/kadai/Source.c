98fca92e4a20015a88e15abde335b624617b5f8b
hotcakeandi <bp16001@shibaura-it.ac.jp>
2018-05-27 22:24:11 +0900

    updated

--- Theme2/5/kadai/Source.c
+++ Theme2/5/kadai/Source.c
@@ -9,14 +9,44 @@ struct hoge
 
 int main() {
 	printf("kadai1\n");
+	/*上から順番に
+	ff
+	ff
+	ff
+	ff
+
+	ff
+	
+	80 1バイトの補数表現
+
+	7f
+	
+	0
+	*/
 	struct hoge d = { -1,255,{-128,127} };
 
+	/*
+	d.bの(先頭)アドレスを格納
+	*/
 	unsigned char *pb = &d.b;
-	
+	/*
+	79 ...y
+	75 ...u
+	61 ...a
+	73 ...s
+	61 ...a
+	00 ...終端文字?
+	*/
 	char name[] = "yuasa";
 
+	/*
+	nameの先頭アドレスを格納
+	*/
 	char *pname = name;
-	
+	/*
+	ff
+	01
+	*/
 	short e[2] = { -1,1 };
 
 
@@ -47,10 +77,6 @@ int main() {
 		printf("%p %x\n", (char *)&e + i, *((char *)&e + i));
 	}
 	printf("\n");
-	int a = 7;
-	for (int  i = 0; i < sizeof(a); i++)
-	{
-		//printf("%p %x\n", ((char *)&a + i), *((char *)&a + i));
-	}
+
 	
 }
