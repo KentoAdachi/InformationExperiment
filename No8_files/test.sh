b8bc2f675b2ecbc347bbd07333394376415e228f
足立賢人 <kent@owlan060188.sras.sic.shibaura-it.ac.jp>
2018-06-21 13:33:24 +0900

    test

--- No8_files/test.sh
+++
@@ -1,3 +0,0 @@
-gcc -Wall -O2 img_proc.c
-time ./a.out hana3.ppm 5
-cmp hana3_res.ppm hana3_res_ans.ppm
--- No8_files/time.bat
+++
@@ -1,4 +0,0 @@
-
-cl img_proc.c
-powershell -NoProfile -ExecutionPolicy Unrestricted .\time.ps1
-fc hana3_res.ppm hana3_res_ans.ppm
